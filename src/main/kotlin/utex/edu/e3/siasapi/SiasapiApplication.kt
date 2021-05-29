64fdd6f6af66e5e920a81faafa7a3a1f9b763685
Juan Alvarez <dssjp90@gmail.com>
2021-05-29 13:22:13 -0500

    v0.1.0 Creacion de proyecto y paquetes

---
+++ src/main/kotlin/utex/edu/e3/siasapi/SiasapiApplication.kt
@@ -0,0 +1,11 @@
+package utex.edu.e3.siasapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SiasapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SiasapiApplication>(*args)
+}
---
+++ src/test/kotlin/utex/edu/e3/siasapi/SiasapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package utex.edu.e3.siasapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SiasapiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

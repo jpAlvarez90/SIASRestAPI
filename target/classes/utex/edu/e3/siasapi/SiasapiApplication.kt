5e5511092f0b7a8277f5dca98cad78404f9f6c28
Danii BC <5228643-Dan09@users.noreply.gitlab.com>
2021-05-29 14:56:50 -0500

    v0.1.1 Inicialización de la rama

--- target/classes/utex/edu/e3/siasapi/SiasapiApplication.kt
+++ target/classes/utex/edu/e3/siasapi/SiasapiApplication.kt
@@ -2,23 +2,10 @@ package utex.edu.e3.siasapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
 class SiasapiApplication
 
 fun main(args: Array<String>) {
     runApplication<SiasapiApplication>(*args)
-
-    
-}
-
-
-@RestController
-class test () {
-
-    @GetMapping("/hello")
-    fun hello(): String = "Hello"
-    
 }
